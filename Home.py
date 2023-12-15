e7fcea6362a02e1e9693e0e06daf293e31313a27
Theerapat patbang <644230041@webmail.npru.ac.th>
2023-12-15 14:51:26 +0700

    .

--- Home.py
+++ Home.py
@@ -25,3 +25,24 @@ import pandas as pd
 
 dt=pd.read_csv('./data/iris.csv')
 st.write(dt.head(10))
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+
+if st.button("show bar chart"):
+    st.bar_chart(dx2)
+    st.button("Not show bar chart")
+else :
+    st.button("Not show bar chart") 
+
+html_2 = """
+<div style="background-color:#FFBF00;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายคลาสดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_2, unsafe_allow_html=True)
+st.markdown("")
