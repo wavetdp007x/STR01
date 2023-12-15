29737f3bc3e8be108c0501176be2a35f0f6fcdab
Theerapat patbang <644230041@webmail.npru.ac.th>
2023-12-15 14:38:55 +0700

    .comit

--- Home.py
+++ Home.py
@@ -9,6 +9,14 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/rung1.jfif')
+    st.image('./pic/01.jpg')
 with col2:
-    st.image('./pic/iris2.jpg')
+    st.image('./pic/02.jpg')
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
