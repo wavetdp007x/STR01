64ae48d420cf8b0b8f4a97970289270e425d0fb6
Theerapat patbang <644230041@webmail.npru.ac.th>
2023-12-15 15:00:42 +0700

    .comit

--- Home.py
+++ Home.py
@@ -72,13 +72,13 @@ if st.button("ทำนายผล"):
    out=Knn_model.predict(x_input)
 
    if out[0]=="Setosa":
-      #st.image("./pic/iris.jpg")
+      st.image("./pic/03.jpg")
       st.header("Setosa")
    elif out[0]=="Versicolor":
-      #st.image("./pic/iris2.jpg")
+      st.image("./pic/04.jpg")
       st.header("Versicolor")
    else:
-      #st.image("./pic/iris1.jpg")  
+      st.image("./pic/05.jpg")  
       st.header("Verginiga")
    st.button("ไม่ทำนายผล")
 else :
